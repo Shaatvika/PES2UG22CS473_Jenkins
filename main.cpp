45cdb39b6c1a4c1bd2d59aa4b352a4c5d8b52b7e
Shaatvika <97600462+Shaatvika@users.noreply.github.com>
2025-03-13 22:33:21 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  cout << "Change for webhook now \n";
+  cout << "We are now executing pipelines \n";
+  return 0;
+}
