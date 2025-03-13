404c6926e4d72ab639b0d7598152c8a231c63ff8
Shaatvika <97600462+Shaatvika@users.noreply.github.com>
2025-03-13 22:20:33 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "Change for webhook now";
+  cout << "Change for webhook now \n";
+  cout << "We are now executing pipelines \n";
   return 0;
 }
